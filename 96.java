0438a93040ef0af36830cd0edaaae56760d2eed5
emmanuelkakumanu <41730018+emmanuelkakumanu@users.noreply.github.com>
2018-08-12 18:00:54 +0530

    AP Sum

---
+++ 96.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		
+		Scanner s=new Scanner(System.in);
+		int a=s.nextInt();
+		int b=s.nextInt();
+		int n=s.nextInt();
+		int sum=a;
+		for(int i=a;n!=0;i=i+b)
+		{
+			sum=sum+i;
+			
+			n=n-1;
+		}
+		System.out.println(sum);
+	}
+}
